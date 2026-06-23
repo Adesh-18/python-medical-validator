91d73bfff9a7eb9a2ef7960b3d6ff5d85fa49dcd
Vajrapu Adesh <adeshadesh645@gmail.com>
2026-06-23 18:17:14 +0530

    Added dict and error handling programs

---
+++ dict.py
@@ -0,0 +1,15 @@
+pizza = {
+    'name': 'Margherita Pizza',
+    'price': 8.9,
+    'calories_per_slice': 250
+}
+
+# print(pizza.keys())
+# print(pizza.values())
+# print(pizza.items())
+# # pizza.clear()
+# # print(pizza)
+# pizza.pop('calories_per_slice',250)
+# print(pizza)
+for price in pizza.keys():
+    print(price)
---
+++ error_handling.py
@@ -0,0 +1,14 @@
+#syntax error This line is missing a closing parenthesis. 
+# Python raises a SyntaxError because the code doesn't follow proper syntax rules.
+#print("Hello, World!"
+#name error This line references an undefined variable 'name', which will raise a NameError.
+#print(name)
+#type error This line attempts to concatenate a string and an integer, which is not allowed in Python and will raise a TypeError.
+#age = 5 + "5"
+#index error This line tries to access an index that is out of range for the list, which will raise an IndexError.
+#numbers = [1, 2, 3]
+#print(numbers[5])
+#attribute error This line tries to call a method that doesn't exist for the list object, which will raise an AttributeError.
+numbers = 42
+numbers.append(4)
+print(numbers)
